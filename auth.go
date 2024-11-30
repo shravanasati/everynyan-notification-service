f3b62a03a4b63a3e857e4e5f68421c5d36c1f482
Shravan Asati <dev.shravan@protonmail.com>
2024-11-30 22:14:56 +0530

    accept ws connections and relay notifications

--- auth.go
+++ auth.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"context"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 
 	_ "github.com/joho/godotenv/autoload"
 	"golang.org/x/crypto/scrypt"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
 )
 
 var secretKey []byte
+var API_KEY string
+var firestoreClient *firestore.Client
 
 func init() {
+	var set bool
+	API_KEY, set = os.LookupEnv("API_KEY")
+	if !set {
+		panic("API_KEY env not set")
+	}
+
 	secretKeyStr, set := os.LookupEnv("SECRET_KEY")
 	if !set {
 		panic("SECRET_KEY env not set")
@@ -31,52 +45,107 @@ func init() {
 	if err != nil {
 		panic("unable to create scrypt secret key: " + err.Error())
 	}
+
+	opt := option.WithCredentialsFile("./serviceAccountKey.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		panic("unable to initialize firebase: " + err.Error())
+	}
+
+	firestoreClient, err = app.Firestore(context.Background())
+	if err != nil {
+		panic("unable to initialize firestore: " + err.Error())
+	}
 }
 
-func decrypt(encryptedText string) (string, error) {
+func decrypt(encryptedText string) ([]byte, error) {
 	encryptedText = strings.ReplaceAll(encryptedText, "%2F", "/")
 	encryptedText = strings.ReplaceAll(encryptedText, "%3D", "=")
 	encryptedText = strings.ReplaceAll(encryptedText, "%2B", "+")
+	emptyByteSlice := []byte{}
 
 	// Decode the base64-encoded data
 	data, err := base64.StdEncoding.DecodeString(encryptedText)
 	if err != nil {
-		return "", err
+		return emptyByteSlice, err
 	}
 
 	// Extract the nonce (12 bytes after the prefix)
-	nonce := data[3:15] // Skip the "v10" prefix
+	nonce := data[3:15]     // Skip the "v10" prefix
 	ciphertext := data[15:] // Rest is ciphertext + auth tag
 
 	// Create AES block
 	block, err := aes.NewCipher([]byte(secretKey))
 	if err != nil {
-		return "", err
+		return emptyByteSlice, err
 	}
 
 	// Create GCM
 	aesGCM, err := cipher.NewGCM(block)
 	if err != nil {
-		return "", err
+		return emptyByteSlice, err
 	}
 
 	// Decrypt the ciphertext
 	plainText, err := aesGCM.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
+		return emptyByteSlice, err
+	}
+
+	return (plainText), nil
+}
+
+type SessionCookie struct {
+	Token string `json:"token"`
+	Role  string `json:"role"`
+}
+
+func getToken(token string) (string, error) {
+	snap, err := firestoreClient.
+		Collection("tokens").
+		Doc(token).
+		Get(context.Background())
+
+	if err != nil {
 		return "", err
 	}
 
-	return string(plainText), nil
+	if !snap.Exists() {
+		return "", fmt.Errorf("token doesnt exist")
+	}
+
+	return snap.Data()["token"].(string), nil
 }
 
-func checkAuth(cookieValue []byte) bool {
+// checkAuth accepts the cookieValue and tries to authenticate the request.
+// if successfull, it returns true and the token value.
+func checkAuth(cookieValue []byte) (bool, string) {
 	decryptedCookie, err := decrypt(string(cookieValue))
 	if err != nil {
 		fmt.Println("unable to decrypt cookie: ", err)
-		return false
+		return false, ""
 	}
 
-	// todo json parse this and then ping firestore or something
-	fmt.Println(decryptedCookie)
-	return true
+	var sessionCookie SessionCookie
+	err = json.Unmarshal([]byte(decryptedCookie), &sessionCookie)
+	if err != nil {
+		fmt.Println("unable to decode cookie into struct", err)
+		return false, ""
+	}
+
+	if sessionCookie.Token == "" {
+		fmt.Println("session cookie token empty")
+		return false, ""
+	}
+
+	dbToken, err := getToken(sessionCookie.Token)
+	if err != nil {
+		fmt.Println("error getting token from db", err)
+		return false, ""
+	}
+	if dbToken != sessionCookie.Token {
+		return false, ""
+	}
+
+	return true, dbToken
 }
--- main.go
+++ main.go
@@ -1,99 +1,157 @@
 package main
 
 import (
-	"errors"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/http"
-	"runtime"
+	"strings"
 
-	"github.com/gobwas/httphead"
+	z "github.com/Oudwins/zog"
 	"github.com/gobwas/ws"
 	"github.com/gobwas/ws/wsutil"
 )
 
-var authorMap = make(map[string]net.Conn)
+type NotificationRequest struct {
+	Users       []string `json:"users,omitempty"`
+	Title       string   `json:"title,omitempty"`
+	Description string   `json:"description,omitempty"`
+}
+
+var notificationRequestSchema = z.Struct(z.Schema{
+	"users":       z.Slice(z.String()).Required(z.Message("users array is required")).Min(1, z.Message("atleast one user is required")),
+	"title":       z.String().Required(z.Message("title is required")).Min(1, z.Message("title cannot be empty")),
+	"description": z.String().Required(z.Message("description is required")).Min(1, z.Message("description cannot be empty")),
+})
+
+var authorConnMap = make(map[string]net.Conn)
 
 func main() {
 	addr := "localhost:7924"
-	ln, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalf("failed to start a tcp server at `%v`, err: %v \n", addr, err)
-	}
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		sessionCookie, err := r.Cookie("session")
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("missing cookies"))
+			return
+		}
 
-	// Prepare handshake header writer from http.Header mapping.
-	header := ws.HandshakeHeaderHTTP(http.Header{
-		"X-Go-Version": []string{runtime.Version()},
+		success, token := checkAuth([]byte(sessionCookie.Value))
+		if !success {
+			w.WriteHeader(http.StatusForbidden)
+			w.Write([]byte("unauthenticated"))
+			return
+		}
+
+		conn, _, _, err := ws.UpgradeHTTP(r, w)
+		if err != nil {
+			log.Println("unable to upgrade the http connection to websocket")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("unable to upgrade the http connection to websocket"))
+			return
+		}
+
+		fmt.Println("new connection: ", conn.RemoteAddr())
+		authorConnMap[token] = conn
+		fmt.Println(authorConnMap)
+		// go func() {
+		// 	defer conn.Close()
+
+		// 	for {
+		// 		msg, op, err := wsutil.ReadClientData(conn)
+		// 		var closedError wsutil.ClosedError
+		// 		if errors.As(err, &closedError) {
+		// 			fmt.Printf("%v broke connection", conn.RemoteAddr())
+		// 			break
+		// 		}
+		// 		if err != nil {
+		// 			fmt.Println("error in reading client data:", err)
+		// 			break
+		// 		}
+		// 		fmt.Println(op, string(msg))
+		// 		wsutil.WriteServerMessage(conn, op, msg)
+		// 	}
+		// }()
 	})
 
-	u := ws.Upgrader{
-		// OnHost: func(host []byte) error {
-		// 	fmt.Println("host:", string(host))
-		// 	if string(host) == "github.com" {
-		// 		return nil
-		// 	}
-		// 	return ws.RejectConnectionError(
-		// 		ws.RejectionStatus(403),
-		// 		ws.RejectionHeader(ws.HandshakeHeaderString(
-		// 			"X-Want-Host: github.com\r\n",
-		// 		)),
-		// 	)
-		// },
-		OnHeader: func(key, value []byte) error {
-			// fmt.Printf("header key=%v value=%v\n", string(key), string(value))
-			if string(key) != "Cookie" {
-				// fmt.Println("skipping because key not cookie")
-				return nil
-			}
-			// todo check for authentication
-			ok := httphead.ScanCookie(value, func(key, value []byte) bool {
-				fmt.Println(string(key), string(value))
-				return checkAuth(value)
-			})
-			if ok {
-				return nil
-			}
-			return ws.RejectConnectionError(
-				ws.RejectionReason("bad cookie"),
-				ws.RejectionStatus(400),
-			)
-		},
-		OnBeforeUpgrade: func() (ws.HandshakeHeader, error) {
-			return header, nil
-		},
-	}
+	router.HandleFunc("POST /trigger", func(w http.ResponseWriter, r *http.Request) {
+		auth := r.Header.Get("Authorization")
+		splittedAuth := strings.Split(auth, " ")
+		if len(splittedAuth) != 2 {
+			log.Println("missing auth")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("missing authorization"))
+			return
+		}
+
+		apiKey := splittedAuth[1]
+		if apiKey != API_KEY {
+			log.Println("invalid api key")
+			w.WriteHeader(http.StatusForbidden)
+			w.Write([]byte("invalid api key"))
+			return
+		}
+
+		reqBody, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println("unable to read request body:", err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("missing request body"))
+			return
+		}
+
+		var notifReq NotificationRequest
+		var reqMap map[string]any
+		err = json.Unmarshal(reqBody, &reqMap)
+		if err != nil {
+			log.Println("unable to unmarshal request body:", err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("missing json in request body"))
+			return
+		}
+
+		errors := notificationRequestSchema.Parse(reqMap, &notifReq)
+		if errors != nil {
+			firstError := errors["$first"]
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(firstError[0].Message()))
+			return
+		}
+
+		fmt.Println("notification request accepted")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("notification request accepted"))
+
+		// todo send to a worker pool
+		for _, user := range notifReq.Users {
+			go func(user string) {
+				userConn, ok := authorConnMap[user]
+				if !ok {
+					return
+				}
+
+				message, err := json.Marshal(map[string]string{
+					"title":       notifReq.Title,
+					"description": notifReq.Description,
+				})
+				if err != nil {
+					log.Panic("unable to jsonify")
+				}
+				err = wsutil.WriteServerText(userConn, message)
+				if err != nil {
+					log.Println("error sending message to client:", err)
+				}
+			}(user)
+		}
+	})
 
 	log.Println("Ready to accept connections at", addr)
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = u.Upgrade(conn)
-		if err != nil {
-			log.Printf("upgrade error: %s", err)
-			continue
-		}
-
-		// todo add connection to a map
-		go func() {
-			defer conn.Close()
-			fmt.Println("new connection: ", conn.RemoteAddr())
-			for {
-				msg, op, err := wsutil.ReadClientData(conn)
-				var closedError wsutil.ClosedError
-				if errors.As(err, &closedError){
-					fmt.Printf("%v broke connection", conn.RemoteAddr())
-					break
-				}
-				if err != nil {
-					fmt.Println("error in reading client data:", err)
-					break
-				}
-				fmt.Println(op, string(msg))
-				wsutil.WriteServerMessage(conn, op, msg)
-			}
-		}()
+	err := http.ListenAndServe(addr, router)
+	if err != nil {
+		log.Fatalf("unable to start a server: %v \n", err)
 	}
+
 }
